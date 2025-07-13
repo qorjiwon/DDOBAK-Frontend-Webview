013ab1d2ae71353b8de07fb6a7d9bb47078dc951
qorjiwon <wldnjsl2001@khu.ac.kr>
2025-07-13 22:42:18 +0900

    fix(image): wrap external SVGs in next/Image and whitelist their host

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+
+  images: {
+    domains: [
+      'm.radio.ytn.co.kr',
+      'www.jipyong.com',
+    ],
+  },
 };
 
 export default nextConfig;
