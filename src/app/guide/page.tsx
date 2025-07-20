ba2c45b5077f73673838e6fbeb893caa9bc5f758
qorjiwon <wldnjsl2001@gmail.com>
2025-07-20 23:24:26 +0900

    feat: add a guide page

---
+++ src/app/guide/page.tsx
@@ -0,0 +1,10 @@
+import '../tips/style.scss';
+import React from 'react';
+
+const ContractClauses: React.FC = () => (
+  <div className="container">
+    <h1 className="heading">또박이 이용 가이드</h1>
+      </div>
+);
+
+export default ContractClauses;
