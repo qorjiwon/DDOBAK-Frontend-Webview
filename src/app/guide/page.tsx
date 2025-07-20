b0afda557135015819d07665ce984864a8f8e3e3
qorjiwon <wldnjsl2001@khu.ac.kr>
2025-07-20 23:40:52 +0900

    chore: add the images in the guide page

--- src/app/guide/page.tsx
+++ src/app/guide/page.tsx
@@ -2,9 +2,35 @@ import '../tips/style.scss';
 import React from 'react';
 
 const ContractClauses: React.FC = () => (
-  <div className="container">
-    <h1 className="heading">또박이 이용 가이드</h1>
-      </div>
+    <div className="container">
+        <h1 className="heading">또박이 이용 가이드</h1>
+        <img 
+            className="guide-image1"
+            src="https://lemon-hockey-ad5.notion.site/image/attachment%3Aa869b186-2a42-41b2-8af6-670f743f1b2b%3ASlide_16_9_-_13.png?table=block&id=236f997e-6c3c-8028-ade2-e886bac83f31&spaceId=105393ee-64eb-465f-a2c3-c903cfb2e132&width=1420&userId=&cache=v2"
+            alt="또박이 이용 가이드 이미지"
+        />
+        <img 
+            className="guide-image2"
+            src="https://lemon-hockey-ad5.notion.site/image/attachment%3Ad1d958c2-726f-47c2-aeae-1d4ea0122d12%3ASlide_16_9_-_1.png?table=block&id=236f997e-6c3c-8088-ace9-c990f02fd80e&spaceId=105393ee-64eb-465f-a2c3-c903cfb2e132&width=1420&userId=&cache=v2"
+            alt="또박이 이용 가이드 이미지"
+        />
+        <img 
+            className="guide-image3"
+            src="https://lemon-hockey-ad5.notion.site/image/attachment%3A39381ec8-c056-463f-9fa9-5622ba4335c2%3ASlide_16_9_-_2.png?table=block&id=236f997e-6c3c-80c6-8e6f-c2ea5eebe659&spaceId=105393ee-64eb-465f-a2c3-c903cfb2e132&width=1420&userId=&cache=v2"
+            alt="또박이 이용 가이드 이미지"
+        />
+        <img 
+            className="guide-image4"
+            src="https://lemon-hockey-ad5.notion.site/image/attachment%3A178f69c8-7bfb-45f0-8294-07cdd3a1f09a%3ASlide_16_9_-_3.png?table=block&id=236f997e-6c3c-80a7-a145-eecc3af0a683&spaceId=105393ee-64eb-465f-a2c3-c903cfb2e132&width=1420&userId=&cache=v2"
+            alt="또박이 이용 가이드 이미지"
+        />
+        <img 
+            className="guide-image5"
+            src="https://lemon-hockey-ad5.notion.site/image/attachment%3A1306a02e-2ea5-4761-8d69-db0edbf9c90c%3ASlide_16_9_-_4.png?table=block&id=236f997e-6c3c-80b0-b57c-ef798a8900ee&spaceId=105393ee-64eb-465f-a2c3-c903cfb2e132&width=1420&userId=&cache=v2"
+            alt="또박이 이용 가이드 이미지"
+        />
+
+    </div>
 );
 
 export default ContractClauses;
